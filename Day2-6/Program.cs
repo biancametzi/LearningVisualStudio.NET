421d68b91750f322ac2fd83e94492619ab69bbd5
biancametzi <bianca.metzi@gmail.com>
2015-12-05 23:31:46 +0200

    More work on day 2

---
+++ Day2-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("Values.txt");
+            string line = string.Empty;
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+            }
+            myReader.Close();
+            Console.ReadLine();            
+        }
+    }
+}
---
+++ Day2-7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int[] numbers = new int[5];
+             numbers[0] = 4;
+             numbers[1] = 8;
+             numbers[2] = 15;
+             numbers[3] = 16;
+             numbers[4] = 23;
+             */
+
+            int[] numbers = { 4, 8, 15, 16, 23 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i].ToString());
+            }
+
+            string[] names = { "Bob", "Bogdan", "Bianca" };
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            string myText = "Now is the time for us to go eat.";
+            char[] charArray = myText.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(charArray);
+
+            //Console.WriteLine(numbers[1].ToString());
+            Console.ReadLine();
+        }
+    }
+}
