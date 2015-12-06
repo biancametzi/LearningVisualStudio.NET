fe8652389ddf6469e8a3820ec558ba7db133ac05
Bianca Metzler <bianca.metzi@gmail.com>
2015-12-06 17:22:36 +0200

    Added more sample code

---
+++ Day0-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day0_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());  // citim o linie de la tastatura si o convertim la un intreg; echivalenta mai jos
+            // string line = Console.ReadLine();                // citim un string de la tastatura
+            // int numberOfLines = int.Parse(line);             // convertim stringul la un numar intreg pe care il vom folosin mai jos la instructiunea 'for'
+
+            StreamWriter sw = new StreamWriter("pisoi.txt");    // deschidem fisierul "pisoi.txt" pentru scriere (daca fisierul nu exista, el va fi creat)
+            for (int i = 0; i < numberOfLines; i++)             // pentru fiecare din cele 'numberOfLines' linii ce urmeaza a fi citite de la tastatura
+            {
+                string line = Console.ReadLine();               // citim o linie de la tastatura
+                sw.WriteLine(line);                             // scriem linia citita de la tastatura in fisierul 'pisoi.txt'
+            }
+
+            sw.Close();
+        }
+    }
+}
---
+++ Day0-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day0_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());      // citim un intreg de la tastatura 
+
+            string[] arrayOfStrings = new string[numberOfLines];    // cream un sir (array) de stringuri de dimensiune 'numberOfLines'
+            for (int i = 0; i < numberOfLines; i++)                 // pentru toate liniile ce urmeaza a fi citite
+            {
+                arrayOfStrings[i] = Console.ReadLine();             // citim liniile de la consola si le stocam/salvam in array-ul 'arrayOfStrings'
+            }
+
+            Array.Reverse(arrayOfStrings);                          // intoarcem array-ul 'arrayOfStrings'
+            StreamWriter sw = new StreamWriter("pisoi.txt");        // deschidem fisirul 'pisoi.txt' pentru scriere
+            for (int i = 0; i < numberOfLines; i++)                 // pentru toate liniile ce le-am citit de la tastatura/consola
+            {
+                sw.WriteLine(arrayOfStrings[i]);                    // scriem in fisierul 'pisoi.txt' arrayOfStrings
+            }
+
+            sw.Close();                                             // inchidem fisierul 'pisoi.txt'
+            Console.ReadLine();                                     // asteptam ca utilizatorul sa inchida consola
+        }
+    }
+}
